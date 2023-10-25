18b4c3b8c73d5b4da94432e6548172c834c13342
テイウンサツ <chengyunce@gmail.com>
2023-10-25 12:15:23 +0900

    1

--- routes.js
+++ routes.js
@@ -6,6 +6,7 @@ const router = express.Router()
 // HomeControllerモジュール読み込み
 const HomeController = require('./controllers/HomeController')
 const ItemController = require('./controllers/ItemController')
+const LoginController = require('./controllers/LoginController')
 
 // GETリクエストの処理
 // Home
@@ -16,27 +17,9 @@ router.get('/profile', HomeController.profile)
 router.get('/item', ItemController.index)
 router.get('/item/:id', ItemController.detail)
 
-// POSTリクエスト
-router.post('/auth', (req, res) => {
-    // POSTデータ取得
-    var loginName = req.body.login_name
-    var password = req.body.password
-    console.log(loginName, password)
-
-    var message = "ログイン失敗"
-    // .env で設定した値で、ログインチェック
-    // TODO：データベースに接続してユーザ取得
-    // TODO：パスワードはハッシュ値でチェック
-    if (loginName == process.env.LOGIN_NAME
-        && password == process.env.PASSWORD) {
-        message = "ログイン成功"
-        //TODO ログインが成功したらユーザの状態を保存
-        //TODO ログイン後のページの転送
-    } else {
-        // TODO ログイン画面に戻す
-    }
-    res.send(message)
-})
+//login
+router.get('/login', LoginController.index)
+router.get('/auth', LoginController.auth)
 
 // モジュール化
 module.exports = router
